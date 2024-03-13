3ea15ec7ad4916ae0a6440be1b6996d8b04aa6ca
T3RM1N4T3D <135352327+T3RM1N4T3D@users.noreply.github.com>
2024-03-13 13:49:25 -0700

    Create eval.js

---
+++ eval.js
@@ -0,0 +1,39 @@
+function buildBlobWithScript(script) {
+    var fullHTML = `<script src="index.js"></script>`;
+    return new Promise((resolve, reject) => {
+        webkitRequestFileSystem(TEMPORARY, 1024 * 1024 * 300, function (fs) {
+            function writeFileInDirectory(dir, name, data) {
+                return new Promise((resolve) => {
+                    dir.getFile(name, { create: true }, function (entry) {
+                        entry.createWriter(function (writer) {
+                            writer.write(new Blob([data]));
+                            writer.onwriteend = function () {
+                                resolve(entry)
+                            }
+                        });
+                    })
+                })
+            }
+            function removeFileInDirectory(dir, name) {
+                return new Promise(function (resolve) {
+                    dir.getFile(name, { create: true }, function (entry) {
+                        entry.remove(resolve);
+                    })
+                })
+            }
+            fs.root.getDirectory('evaluations', { create: true }, async function (entry) {
+                await removeFileInDirectory(entry, 'index.js');
+                await writeFileInDirectory(entry, 'index.js', script);
+                await removeFileInDirectory(entry, 'index.html');
+                var handle = await writeFileInDirectory(entry, 'index.html', fullHTML);
+                resolve(handle.toURL());
+
+            }, reject)
+        }, reject)
+    })
+}
+document.querySelector('button').onclick = async () => {
+    var url = await buildBlobWithScript(document.querySelector('textarea').value);
+    // unbelievable, why can't we just use open
+    await chrome.tabs.create({ url: url })
+};
